77284a09310b2a0eaa8898801ae933d476b94bb4
venkattrj <venkatramcool001@gmail.com>
2015-11-21 02:07:01 +0530

    Worst

--- My-App-PortFolio/app/src/main/java/com/example/android/my_app_portfolio/MainActivity.java
+++ My-App-PortFolio/app/src/main/java/com/example/android/my_app_portfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.my_app_portfolio;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,62 +9,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1 = (Button) this.findViewById(R.id.button);
-        btn1.setOnClickListener((View.OnClickListener) this);
 
-        Button btn2 = (Button) this.findViewById(R.id.button2);
-        btn2.setOnClickListener((View.OnClickListener) this);
 
-        Button btn3 = (Button) this.findViewById(R.id.button3);
-        btn3.setOnClickListener((View.OnClickListener) this);
-
-        Button btn4 = (Button) this.findViewById(R.id.button3);
-        btn4.setOnClickListener((View.OnClickListener) this);
-
-        Button btn5 = (Button) this.findViewById(R.id.button4);
-        btn5.setOnClickListener((View.OnClickListener) this);
-
-        Button btn6 = (Button) this.findViewById(R.id.button5);
-        btn6.setOnClickListener((View.OnClickListener) this);
-
-        Button btn7 = (Button) this.findViewById(R.id.button6);
-        btn7.setOnClickListener((View.OnClickListener) this);
-    }
-
-    public void OnClick(View v) {
-        switch (v.getId()) {
-            case R.id.button:
-                Toast.makeText(getApplicationContext(), "This button will launch Spotify Streamer", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.button2:
-                Toast.makeText(getApplicationContext(), "This button will launch Scores App", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.button3:
-                Toast.makeText(getApplicationContext(), "This button will launch Library App", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.button4:
-                Toast.makeText(getApplicationContext(), "This button will launch BuildIt Bigger App", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.button5:
-                Toast.makeText(getApplicationContext(), "This button will launch XYZ Reader App", Toast.LENGTH_LONG).show();
-                break;
-
-            case R.id.button6:
-                Toast.makeText(getApplicationContext(), "This button will launch Capstone Project App", Toast.LENGTH_LONG).show();
-                break;
+        View btn3 = (Button) this.findViewById(R.id.button3);
+        btn3.setOnClickListener((View.OnClickListener)this);
 
 
 
+
+
+        public void OnClickListener(View v) {
+            switch (v.getId()) {
+                case R.id.button:
+                    Toast.makeText(getApplicationContext(), "This button will launch Spotify Streamer", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.button2:
+                    Toast.makeText(getApplicationContext(), "This button will launch Scores App", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.button3:
+                    Toast.makeText(getApplicationContext(), "This button will launch Library App", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.button4:
+                    Toast.makeText(getApplicationContext(), "This button will launch BuildIt Bigger App", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.button5:
+                    Toast.makeText(getApplicationContext(), "This button will launch XYZ Reader App", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.button6:
+                    Toast.makeText(getApplicationContext(), "This button will launch Capstone Project App", Toast.LENGTH_LONG).show();
+                    break;
+
+
+
+            }
         }
     }
+
+
 }
