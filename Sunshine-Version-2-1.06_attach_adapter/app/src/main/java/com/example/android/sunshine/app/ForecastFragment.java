a58fdef30b06ec715a42139825a6bc2c48d57d88
venkattrj <venkatramcool001@gmail.com>
2015-12-07 09:52:01 +0530

    Sunshine

--- Sunshine-Version-2-1.06_attach_adapter/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ Sunshine-Version-2-1.06_attach_adapter/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -56,7 +58,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()== R.id.refresh_bth){
-            weatherTask wt =new weatherTask();
+            weatherTask wt = new weatherTask();
             wt.execute("Madurai");
             return true;
         }
@@ -95,8 +97,15 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String forcast = mForecastAdapter.getItem(i);
+                Toast.makeText(getActivity(), forcast, Toast.LENGTH_LONG).show();
+            }
+        });
 
-        return rootView;
+        return  rootView;
     }
 
     public  class weatherTask extends AsyncTask<String,Void,String[]> {
